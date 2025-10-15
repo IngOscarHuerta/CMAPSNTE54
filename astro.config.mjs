fa2a83fd69a7b7ea92a0744f369ea21344896c68
IngOscarHuerta <inginfohuerta@gmail.com>
2025-10-15 12:15:14 -0700

    Fix produccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel'; 
 
 // https://astro.build/config
 export default defineConfig({
   // Dominio público para URLs absolutas (requerido por sitemap y canónicos)
   site: 'https://www.cmapsnte54.com.mx/',
-  output: 'server', // 'hybrid' también vale
-  adapter: vercel(),
+  output: 'static', // Asegura HTML estático
   // Integraciones
   integrations: [
     sitemap(), // genera /sitemap-index.xml en build
